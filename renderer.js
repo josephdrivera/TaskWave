662b31715915840fe494a83aaa6df704ac85fe23
Joseph D Rivera <josephdrivera15@gmail.com>
2023-11-30 09:00:20 -0500

    update it with the file and switched over to local stores for right now

--- renderer.js
+++ renderer.js
@@ -3,16 +3,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const taskInput = document.getElementById('new-task'); // Get the input field
     const taskList = document.getElementById('task-list'); // Get the task list
 
+    // Function to save tasks to localStorage
+    function saveTasks(tasks) {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+
+    // Function to load tasks from localStorage
+    function loadTasks() {
+        const tasks = localStorage.getItem('tasks');
+        return tasks ? JSON.parse(tasks) : [];
+    }
+
     // Function to add task
     function addTask(taskContent) {
-        window.electronAPI.addTask(taskContent, (err, docRefId) => {
-            if (err) {
-                console.error("Error adding task:", err);
-            } else {
-                console.log("Task added with ID:", docRefId);
-                displayTask(taskContent); // Update the UI
-            }
-        });
+        const tasks = loadTasks();
+        tasks.push({ content: taskContent });
+        saveTasks(tasks);
+        displayTask(taskContent); // Update the UI
     }
 
     // Function to display a single task on the UI
@@ -24,10 +31,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Function to load and display all tasks
     function loadAndDisplayTasks() {
-        window.electronAPI.getTasks((tasks) => {
-            taskList.innerHTML = ''; // Clear the task list before loading
-            tasks.forEach(task => displayTask(task.content));
-        });
+        const tasks = loadTasks();
+        taskList.innerHTML = ''; // Clear the task list before loading
+        tasks.forEach(task => displayTask(task.content));
     }
 
     loadAndDisplayTasks();
